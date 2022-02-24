504e6b20f8e4486b902d34916fe3351e1a49e8f0
Pappyjay23 <pjinadu23@gmail.com>
2022-02-24 15:02:05 +0100

    Tweaked the countdown functionality

--- src/components/LaunchPage.js
+++ src/components/LaunchPage.js
@@ -3,105 +3,85 @@ import "./LaunchPage.css"
 import {FaFacebook, FaInstagram, FaPinterest} from "react-icons/fa"
 
 const LaunchPage = () => {
-    // States
-    let [days, setDays] = useState('08')
-    const [hours, setHours] = useState('23')
-    const [minutes, setMinutes] = useState('59')
-    const [seconds, setSeconds] = useState('59')
-
-    const dayInterv = () =>{
-        let start = 8;
-        const end = 0
-
-        if (start === end) return; 
-
-        let timer = setInterval(() => {
-          start -= 1;
-          setDays(start)
-          if (start === end) clearInterval(timer)       
-        }, 86400000);
-    }
-
-    const hourInterv = () =>{
-        let start = 23;
-        const end = 0
-
-        if (start === end) return; 
-
-        let timer = setInterval(() => {
-          start -= 1;
-          setHours(start)
-          if (start === end) setInterval(hourInterv())       
-        }, 3600000);
-    }
+    const [complete, setComplete] = useState(false)
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
     
-    const minuteInterv = () =>{
-        let start = 59;
-        const end = 0
+    function calculateTimeLeft() {  
 
-        if (start === end) return; 
+        // let days = 8;
+        // let newDate = new Date(Date.now("00:00:00:00") + days * 24*60*60*1000);
+        // const now = new Date()
+        // const difference = newDate - now
+        // console.log(now)
+        // console.log(newDate)
 
-        let timer = setInterval(() => {
-          start -= 1;
-          setMinutes(start)
-          if (start === end) setInterval(minuteInterv())       
-        }, 60000);
+        const year = new Date().getFullYear();
+        // const month = new Date().getMonth();
+        // const day = new Date().getDay() + 7;
+        const difference = +new Date(`3/7/${year}`) - +new Date();
+        let timeLeft = {};
+
+        
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+                minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+                seconds: Math.floor((difference % (1000 * 60)) / 1000)
+            };
+            return timeLeft
+        }else{
+            setComplete(true)
+        }
     }
 
-    const secondInterv = () =>{
-        let start = 59;
-        const end = 0
 
-        if (start === end) return; 
-
-        let timer = setInterval(() => {
-          start -= 1;
-          setSeconds(start)
-          if (start === end) setInterval(secondInterv())       
+    useEffect(() => {
+        const timer = setTimeout(() => {
+        setTimeLeft(calculateTimeLeft());
         }, 1000);
-    }
-   
-    useEffect(() => { 
-        secondInterv();
-        minuteInterv();  
-        hourInterv();
-        dayInterv();
-      }, []);
+
+        return () => {
+        clearTimeout(timer);
+        };
+    });
 
 
 
   return (
     <div className='launch-page'>
         <div className="launch-content">
-
             <h1 className='launch-title'>We're Launching soon</h1>
-
-            <div className="clock">
-                <div className="countdown-box">
-                    <div className="box-item">
-                        <p>{days}</p>
+            {complete ? 
+                <h1 className='time-complete'>Time Up!!</h1>
+                :
+                <div className="clock">
+                    <div className="countdown-box">
+                        <div className="box-item">
+                            <p>{timeLeft.days}</p>
+                        </div>
+                        <p>Days</p>
                     </div>
-                    <p>Days</p>
-                </div>
-                <div className="countdown-box">
-                    <div className="box-item">
-                        <p>{hours}</p>
+                    <div className="countdown-box">
+                        <div className="box-item">
+                            <p>{timeLeft.hours}</p>
+                        </div>
+                        <p>Hours</p>
                     </div>
-                    <p>Hours</p>
-                </div>
-                <div className="countdown-box">
-                    <div className="box-item">
-                        <p>{minutes}</p>
+                    <div className="countdown-box">
+                        <div className="box-item">
+                            <p>{timeLeft.minutes}</p>
+                        </div>
+                        <p>Minutes</p>
                     </div>
-                    <p>Minutes</p>
-                </div>
-                <div className="countdown-box">
-                    <div className="box-item">
-                        <p>{seconds}</p>
+                    <div className="countdown-box">
+                        <div className="box-item">
+                            <p>{timeLeft.seconds}</p>
+                        </div>
+                        <p>Seconds</p>
                     </div>
-                    <p>Seconds</p>
-                </div>
-            </div>
+                </div>    
+            }
 
             <div className="social-icons">
                 <FaFacebook className='socials' />
